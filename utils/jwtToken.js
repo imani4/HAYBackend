488d3a498737af636dee907ed2654c24bea8b3b3
imani4 <143686154+imani4@users.noreply.github.com>
2023-12-20 18:31:38 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,14 +4,16 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
   // options for cookie
-  const options = {
-    expires: new Date(
-      Date.now() +  12* 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
+  // const options = {
+  //   expires: new Date(
+  //     Date.now() +  12* 24 * 60 * 60 * 1000
+  //   ),
+  //   httpOnly: true,
+  // };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode)
+    // .cookie("token", token, options)
+    .json({
     success: true,
     user,
     token,
